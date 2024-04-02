501f3f9ee34b992bfc6ed5b0b8ef9736ddb03448
Andreas <xnorcode@gmail.com>
2024-04-02 11:44:09 +0100

    refactor: corrected dir

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--api --xformers --enable-insecure-extension-access --theme dark --ckpt /home/ubuntu/sd-webui/models/Stable-diffusion/juggernautXL_v9Rdphoto2Lightning.safetensors --no-half-vae"
+export COMMANDLINE_ARGS="--api --xformers --enable-insecure-extension-access --theme dark --ckpt /home/ubuntu/stable-diffusion/stable-diffusion-webui/models/Stable-diffusion/juggernautXL_v9Rdphoto2Lightning.safetensors --no-half-vae"
 
 # python3 executable
 #python_cmd="python3"
